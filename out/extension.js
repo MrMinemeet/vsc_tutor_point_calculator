749ea532f36d9506526177ba6d57ae10b6d52311
MrMinemeet <alexander.voglsperger@proton.me>
2022-11-24 17:31:03 +0100

    Added keyboard shortcut
    Keybinding is "CTRL + F3"

--- out/extension.js
+++ out/extension.js
@@ -16,8 +16,6 @@ function activate(context) {
     // The commandId parameter must match the command field in package.json
     const disposable = vscode.commands.registerCommand('tutor-point-calculator.calculatepoints', () => {
         // The code you place here will be executed every time your command is executed
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hi');
         // Get current workspace
         let files = fetchFiles(getWorkspace());
         // Filter files to only include .java files
