a0ff88867789ddd9fb3c989bf180c53e4ff5ff6b
MrMinemeet <alex@wtf-my-code.works>
2023-10-02 09:52:59 +0200

    Adjust linting
    Make eslint check for a few more things to make the code look nicer

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+# Lint before
+npx eslint . &&
+# && makes vsce only execute if eslint was successful
+
+
 # Build to vsix
 vsce package
--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,7 @@ import fs = require('fs');
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: vscode.ExtensionContext): void {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
